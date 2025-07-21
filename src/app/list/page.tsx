113b76300933a7ae548ce87a923f72d0ed4b6fd8
davidagena <159203783+davidagena@users.noreply.github.com>
2025-07-20 14:53:13 -1000

    Cleaning

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,13 +1,40 @@
 import { getServerSession } from 'next-auth';
 import { Col, Container, Row } from 'react-bootstrap';
-import { prisma } from '@/lib/prisma';
 import { loggedInProtectedPage } from '@/lib/page-protection';
 import authOptions from '@/lib/authOptions';
 import ContactCard from '@/components/ContactCard';
-import ContactCardAdmin from '@/components/ContactCardAdmin'; // Optional if ADMIN is supported
-import { Role } from '@prisma/client';
 
-/** Render a list of contacts for the logged in user. */
+const contacts = [
+  {
+    firstName: 'Philip',
+    lastName: 'Johnson',
+    address: 'POST 307, University of Hawaii',
+    image: 'https://github.com/philipmjohnson.png',
+    description:
+      // eslint-disable-next-line max-len
+      'I am a Professor of Information and Computer Sciences at the University of Hawaii, Director of the Collaborative Software Development Laboratory, and the CEO of OpenPowerQuality.com.',
+  },
+  {
+    firstName: 'Henri',
+    lastName: 'Casanova',
+    address: 'POST 307, University of Hawaii',
+    image: 'https://avatars0.githubusercontent.com/u/7494478?s=460&v=4',
+    description:
+      // eslint-disable-next-line max-len
+      'I am originally from France. I maintain a list of reports from my surf sessions. I have proof that I ran the Hana relay with an actual Team.',
+  },
+  {
+    firstName: 'Kim',
+    lastName: 'Binsted',
+    address: 'POST 307, University of Hawaii',
+    image: 'https://www.ics.hawaii.edu/wp-content/uploads/2013/08/kim_binsted-square-300x300.jpg',
+    description:
+      // eslint-disable-next-line max-len
+      'Kim Binsted received her BSc in Physics at McGill (1991), and her PhD in Artificial Intelligence from the University of Edinburgh (1996). Her thesis topic was the computational modeling and generation of punning riddles, and her program, JAPE (Joke Analysis and Production Engine), generated puns such as "What do you call a Martian who drinks beer? An ale-ien!".',
+  },
+];
+
+/** Render a list of hardcoded contacts */
 const ListPage = async () => {
   // Protect the page
   const session = await getServerSession(authOptions);
@@ -17,44 +44,20 @@ const ListPage = async () => {
     } | null,
   );
 
-  const owner = session?.user?.email || '';
-
-  // Get user role (optional: only needed if you want to support ADMIN views)
-  const user = await prisma.user.findUnique({
-    where: { email: owner },
-  });
-
-  // Get all contacts owned by this user
-  const contacts = await prisma.contact.findMany({
-    where: { owner },
-  });
-
-  // Get all notes owned by this user
-  const notes = await prisma.note.findMany({
-    where: { owner },
-  });
-
   return (
     <main>
       <Container id="list" fluid className="py-3">
         <Row>
           <Col>
-            <h1>List Contacts</h1>
+            <h1 className="text-center">List Contacts</h1>
           </Col>
         </Row>
         <Row xs={1} md={2} lg={3} className="g-4">
-          {contacts.map((contact) => {
-            const contactNotes = notes.filter(note => note.contactId === contact.id);
-            return (
-              <Col key={`Contact-${contact.id}`}>
-                {user?.role === Role.ADMIN ? (
-                  <ContactCardAdmin contact={contact} notes={contactNotes} />
-                ) : (
-                  <ContactCard contact={contact} notes={contactNotes} />
-                )}
-              </Col>
-            );
-          })}
+          {contacts.map((contact, index) => (
+            <Col key={`Contact-${index}`}>
+              <ContactCard contact={contact} notes={[]} />
+            </Col>
+          ))}
         </Row>
       </Container>
     </main>
